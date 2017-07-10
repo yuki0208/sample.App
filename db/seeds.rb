9cb1beee0499d9409f4d53262390371fc2d5d9f5
yuki <yukkixsu8@yahoo.co.jp>
2017-07-10 22:50:06 +0900

    フォロー,アンフォローの表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Faker::Config.locale = :ja
 
-User.create!(name:  "Example User",
+User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
@@ -25,3 +25,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
